7ad201fc21de18f2313ab32a7746a387f3eb036f
KamilZakolski <zakolski@outlook.com>
2024-06-13 15:05:32 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiExample.Models;
+using RestApiExample.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiExample.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public UsersController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.CurrentFilter = searchString;
+
+            var users = await _apiService.GetUsersAsync();
+
+            // Filtracja danych
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(u => u.Name.Contains(searchString, System.StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            // Sortowanie danych
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    users = users.OrderByDescending(u => u.Name).ToList();
+                    break;
+                case "email":
+                    users = users.OrderBy(u => u.Email).ToList();
+                    break;
+                case "email_desc":
+                    users = users.OrderByDescending(u => u.Email).ToList();
+                    break;
+                default:
+                    users = users.OrderBy(u => u.Name).ToList();
+                    break;
+            }
+
+            return View(users);
+        }
+    }
+}
---
+++ Services/ApiService.cs
@@ -0,0 +1,27 @@
+﻿using RestApiExample.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestApiExample.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<User>>(content);
+        }
+    }
+}
